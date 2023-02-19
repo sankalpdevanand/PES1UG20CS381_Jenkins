bd2eb0d98ca3a1ed7f7d7693115ddb5da38212fa
sankalpdevanand <83156034+sankalpdevanand@users.noreply.github.com>
2023-02-19 22:46:02 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, hi i'm Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built and run the new change in code\n";
   return 0;
 }
